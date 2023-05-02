6ca6810f48247a60a4d9229f6514f19b165ddbaf
dinaraparanid <dinaraparanid@gmail.com>
2023-05-02 15:38:27 +0300

    k(v)

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "agla/predator_prey.hpp"
 
 int main() {
-	std::random_device random_device;
+	/*std::random_device random_device;
 	std::mt19937 rng(random_device());
 	std::uniform_int_distribution<std::size_t> int_generator(20, 50);
 	std::uniform_real_distribution<long double> double_generator(0.1, 1.0);
@@ -36,13 +36,6 @@ int main() {
 	for (std::size_t i = 1; i <= points; ++i)
 		times[i] = time_step * i;
 
-	std::puts("t:");
-
-	for (const auto& t : times)
-		std::printf("%Lf ", t);
-
-	std::putchar('\n');
-
 	const long double k0 = killers_num - a1 / b1;
 	const long double v0 = victims_num - a2 / b2;
 
@@ -51,25 +44,15 @@ int main() {
 	for (std::size_t i = 0; i <= points; ++i)
 		victims_coefficients[i] = predator_prey::V(times[i], a1, b1, a2, b2, v0, k0);
 
-	std::puts("v:");
-
-	for (const auto& v : victims_coefficients)
-		std::printf("%Lf ", v);
-
-	std::putchar('\n');
-
 	std::vector<long double> killers_coefficients(points + 1);
 
 	for (std::size_t i = 0; i <= points; ++i)
 		killers_coefficients[i] = predator_prey::K(times[i], a1, b1, a2, b2, v0, k0);
 
-	std::puts("k:");
+	std::puts("Time | V(T) | K(T)");
 
-	for (const auto& v : killers_coefficients)
-		std::printf("%Lf ", v);
-
-
-	std::putchar('\n');
+	for (std::size_t i = 0; i <= points; ++i)
+		std::printf("%.2Lf %.2Lf %.2Lf\n", times[i], victims_coefficients[i], killers_coefficients[i]);
 
 	char v_eq_buf[1000]; std::sprintf(
 		v_eq_buf,
@@ -122,6 +105,39 @@ int main() {
 		.set_style("lines")
 		.plot_equation(k_equation, "k(t)")
 		.showonscreen();
+
+	plot
+		.reset_all()
+		.savetops("killers_to_victims")
+		.set_title("Killers to Victims")
+		.set_grid()
+		.set_style("lines")
+		.set_xlabel("victims")
+		.set_ylabel("killers")
+		.set_style("points")
+		.plot_xy(victims_coefficients, killers_coefficients)
+		.showonscreen();*/
+
+	std::puts("<table>");
+
+	std::puts("\t<tr>");
+	std::puts("\t\t<th>Time</th>");
+	std::puts("\t\t<th>V(T)</th>");
+	std::puts("\t\t<th>K(T)</th>");
+	std::puts("\t</tr>");
+
+	for (int i = 0; i <= 46; ++i) {
+		long double t = 0, v = 0, k = 0;
+		std::scanf("%Lf%Lf%Lf", &t, &v, &k);
+
+		std::puts("\t<tr>");
+		std::printf("\t\t<td>%.2Lf</td>\n", t);
+		std::printf("\t\t<td>%.2Lf</td>\n", v);
+		std::printf("\t\t<td>%.2Lf</td>\n", k);
+		std::puts("\t</tr>");
+	}
+
+	std::puts("</table>");
 
 	return 0;
 }
